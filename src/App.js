f191b5356731f0d77bde44c7c3c6ebbf80bea22f
abiraltamang <abiral.tamang816@gmail.com>
2022-07-24 21:18:04 +0545

    ..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Carousel from "./components/Carousel";
+import Register from "./components/user/Register";
 
 function App() {
   return (
-    <div className="relative">
-      <Navbar />
-      <Carousel />
+    <div className="">
+      <Register />
+      {/* <Navbar />
+      <Carousel /> */}
     </div>
   );
 }
